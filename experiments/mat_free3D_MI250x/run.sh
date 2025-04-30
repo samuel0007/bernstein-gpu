2d3bd343602461a11e98d86284b82ec61097b7b5
samuel0007 <russo.samuel20@gmail.com>
2025-04-30 14:32:44 +0300

    mi250x 64bit data

--- experiments/mat_free3D_MI250x/run.sh
+++ experiments/mat_free3D_MI250x/run.sh
@@ -1,9 +1,10 @@
 EXPERIMENT=mat_free3D_MI250x
+SCALAR_TYPE=float64
 
 nelements=10
 for p in 3 4 5 6 7 8 9; do
     cd build
-    cmake .. -Dpolynomial_degree=$p -DCMAKE_CXX_FLAGS="-I${MPICH_DIR}/include"   -DCMAKE_EXE_LINKER_FLAGS="-L${MPICH_DIR}/lib -lmpi ${PE_MPICH_GTL_DIR_amd_gfx90a} ${PE_MPICH_GTL_LIBS_amd_gfx90a}"
+    cmake .. -Dscalar_type=$SCALAR_TYPE -Dpolynomial_degree=$p -DCMAKE_CXX_FLAGS="-I${MPICH_DIR}/include"   -DCMAKE_EXE_LINKER_FLAGS="-L${MPICH_DIR}/lib -lmpi ${PE_MPICH_GTL_DIR_amd_gfx90a} ${PE_MPICH_GTL_LIBS_amd_gfx90a}"
     make mat_free_mass3D
     cd ..
     srun --ntasks=1  build/mat_free_mass3D --nelements $nelements --nreps 500 2>&1 | tee experiments/$EXPERIMENT/log_$p.txt
