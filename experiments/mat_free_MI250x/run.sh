9b9bbf0f79cd5caaf98f702ff2d3e0028abc5d76
samuel0007 <russo.samuel20@gmail.com>
2025-05-25 12:14:11 +0000

    mi250x run scripts

--- experiments/mat_free_MI250x/run.sh
+++ experiments/mat_free_MI250x/run.sh
@@ -1,4 +1,4 @@
-EXPERIMENT=mat_free3D_MI250x
+EXPERIMENT=mat_free_MI250x
 
 module load LUMI/23.09
 module load partition/G
@@ -13,13 +13,14 @@ spack load cmake
 export CC=gcc-12
 export CXX=g++-12
 
-nelements=10
+
+nelements=256
 for s in float32 float64; do
     for p in 2 3 4 5 6 7 8 9; do
         cd build
         cmake .. -Dscalar_type=$s -Dpolynomial_degree=$p -Dnvidia=Off -Damd=On -DCMAKE_CXX_FLAGS="-I${MPICH_DIR}/include"   -DCMAKE_EXE_LINKER_FLAGS="-L${MPICH_DIR}/lib -lmpi ${PE_MPICH_GTL_DIR_amd_gfx90a} ${PE_MPICH_GTL_LIBS_amd_gfx90a}"
-        make mat_free_mass3D
+        make mat_free_mass
         cd ..
-	mpirun -n 1 build/mat_free_mass3D --nreps 500 --nelements $nelements 2>&1 | tee experiments/$EXPERIMENT/log_${s}_$p.txt
+	mpirun -n 1 build/mat_free_mass --nreps 500 --nelements $nelements 2>&1 | tee experiments/$EXPERIMENT/log_${s}_$p.txt
     done
 done
